2812cdfecd7f6baa12ff20719fce14aa5cd6690d
spal-synaptics <sauryadeep.pal@synaptics.com>
2024-09-06 10:16:16 -0700

    add default mask colors for more visual consistency

--- image_od.py
+++ image_od.py
@@ -22,6 +22,89 @@ import random
 import os.path
 import numpy as np
 
+default_mask_colors: dict[str, list[int]] = {
+    0: [189, 114, 0],       # Blue
+    1: [25, 83, 217],       # Orange
+    2: [32, 177, 237],      # Yellow
+    3: [142, 47, 126],      # Purple
+    4: [48, 172, 119],      # Green
+    5: [238, 190, 77],      # Light Blue
+    6: [47, 20, 162],       # Dark Red
+    7: [77, 77, 77],        # Gray
+    8: [153, 153, 153],     # Light Gray
+    9: [0, 0, 255],         # Red
+    10: [0, 128, 255],      # Orange
+    11: [0, 191, 191],      # Olive
+    12: [0, 255, 0],        # Green
+    13: [255, 0, 0],        # Blue
+    14: [255, 0, 170],      # Purple
+    15: [0, 85, 85],        # Dark Olive
+    16: [0, 170, 85],       # Olive Green
+    17: [0, 255, 85],       # Light Green
+    18: [0, 85, 170],       # Brown
+    19: [0, 170, 170],      # Dark Yellow
+    20: [0, 255, 170],      # Lime
+    21: [0, 85, 255],       # Orange Red
+    22: [0, 170, 255],      # Light Orange
+    23: [0, 255, 255],      # Yellow
+    24: [128, 85, 0],       # Teal
+    25: [128, 170, 0],      # Dark Cyan
+    26: [128, 255, 0],      # Aquamarine
+    27: [85, 0, 128],       # Indigo
+    28: [85, 85, 128],      # Slate Blue
+    29: [85, 170, 128],     # Light Sea Green
+    30: [85, 255, 128],     # Medium Sea Green
+    31: [170, 0, 128],      # Dark Magenta
+    32: [170, 85, 128],     # Medium Orchid
+    33: [170, 170, 128],    # Khaki
+    34: [170, 255, 128],    # Pale Green
+    35: [255, 0, 128],      # Deep Pink
+    36: [255, 85, 128],     # Hot Pink
+    37: [255, 170, 128],    # Light Coral
+    38: [255, 255, 128],    # Light Yellow
+    39: [255, 85, 0],       # Dodger Blue
+    40: [255, 170, 0],      # Deep Sky Blue
+    41: [255, 255, 0],      # Cyan
+    42: [128, 0, 85],       # Medium Blue
+    43: [128, 85, 85],      # Slate Blue
+    44: [128, 170, 85],     # Steel Blue
+    45: [128, 255, 85],     # Light Cyan
+    46: [170, 0, 85],       # Purple
+    47: [170, 85, 85],      # Medium Purple
+    48: [170, 170, 85],     # Lavender
+    49: [170, 255, 85],     # Pale Turquoise
+    50: [255, 0, 85],       # Magenta
+    51: [255, 85, 85],      # Orchid
+    52: [255, 170, 85],     # Plum
+    53: [255, 255, 255],    # White
+    54: [0, 0, 85],         # Dark Red
+    55: [0, 0, 128],        # Maroon
+    56: [0, 0, 170],        # Firebrick
+    57: [0, 0, 212],        # Crimson
+    58: [0, 0, 255],        # Red
+    59: [0, 85, 0],         # Dark Green
+    60: [0, 128, 0],        # Green
+    61: [0, 170, 0],        # Forest Green
+    62: [0, 212, 0],        # Lime Green
+    63: [0, 255, 0],        # Lime
+    64: [85, 0, 0],         # Midnight Blue
+    65: [128, 0, 0],        # Navy
+    66: [170, 0, 0],        # Dark Blue
+    67: [212, 0, 0],        # Medium Blue
+    68: [255, 0, 0],        # Blue
+    69: [85, 85, 0],        # Olive Drab
+    70: [0, 85, 128],       # Peru
+    71: [0, 85, 170],       # Chocolate
+    72: [0, 85, 212],       # Dark Orange
+    73: [0, 85, 255],       # Orange Red
+    74: [0, 128, 85],       # Olive Green
+    75: [0, 128, 128],      # Dark Khaki
+    76: [0, 128, 170],      # Goldenrod
+    77: [0, 128, 212],      # Dark Goldenrod
+    78: [0, 128, 255],      # Orange
+    79: [0, 170, 85],       # Dark Olive Green
+}
+
 def get_rand_color():
     return [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
 
