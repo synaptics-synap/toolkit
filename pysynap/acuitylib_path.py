b4941799c617b86f25279b7af4deaa65f4f62280
Meet-synaptics <156004208+Meet-synaptics@users.noreply.github.com>
2024-09-10 08:23:22 -0700

    add pip configuration (#3)
    * Toolkit 3.1.1 - Added pip support
    
    * modify workflow
    
    * Simplified requirement.txt
    - Updated wheel package name to conform with PEP440 naming standards.
    - Update install_require for wheel package
    
    * Simplified requiremnet.txt
    - Update wheel package name as per PEP440 Naming standards
    - Update install_require for wheel package
    - Update github action to upload wheels in specified draft release
    
    * Update workflow
    
    - Remove deprecation warning for Node.js
    - Build only binary distribution
    
    * Added 'image_od', 'image_from_raw', and 'image_to_raw' tools to the wheel
    - Excluded unnecessary files from the wheel
    - Incremented toolkit version
    - Added console script for the 'help' command
    
    * Modify help console script

--- pysynap/acuitylib_path.py
+++ pysynap/acuitylib_path.py
@@ -5,12 +5,16 @@ import os
 import sys
 import pathlib
 
-toolkit_dir = pathlib.Path(__file__).parent.parent.absolute()
+current_path = pathlib.Path(__file__)
+toolkit_dir = current_path.parent.parent.absolute()
 internal_tree = False
 # check if pysynap is run from the source tree
 vsi_acuity_dir = f"{toolkit_dir}/../../acuity"
 if os.path.exists(f"{vsi_acuity_dir}/acuitylib/__init__.py"):
     internal_tree = True
+#check if prebuilts tree is available in current directory
+elif os.path.exists(f"{current_path.parent.absolute()}/prebuilts/acuitylib/__init__.py"):
+    vsi_acuity_dir  = f"{current_path.parent.absolute()}/prebuilts/"
 else:
     # Check if toolkit-prebuilts tree is available alongside
     vsi_acuity_dir  = f"{toolkit_dir}/../toolkit-prebuilts"
